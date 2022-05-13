f86dda25e09e9506b1222421f3d7b57f9aae8aa2
evilieswithin <akshayurankar48@gmail.com>
2022-05-13 15:05:14 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiZXZpbGllc3dpdGhpbiIsImEiOiJjbDM0MXNwenUwenM5M2lwOWxuMmJzZnRtIn0.SRbbfRnehnqWe5QhluZUnw",
+  },
+};
