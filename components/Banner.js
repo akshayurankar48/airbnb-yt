cdd7845b7314ff208fb92ad61b3b2faefbdbdac3
evilieswithin <akshayurankar48@gmail.com>
2022-05-13 15:21:26 +0530

    changes

--- components/Banner.js
+++ components/Banner.js
@@ -7,6 +7,7 @@ const Banner = () => {
         layout="fill"
         src="https://links.papareact.com/0fm"
         objectFit="cover"
+        alt=""
       />
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
--- components/MediumCard.js
+++ components/MediumCard.js
@@ -4,7 +4,7 @@ const MediumCard = ({ image, title }) => {
   return (
     <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
       <div className="relative h-80 w-80">
-        <Image className="rounded-xl" src={image} layout="fill" />
+        <Image className="rounded-xl" src={image} layout="fill" alt="" />
       </div>
       <h3 className="text-2xl mt-3">{title}</h3>
     </div>
